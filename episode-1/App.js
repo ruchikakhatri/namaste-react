51a7f1ab02afa07f5691501f7563b064bf40f797
Dharmendra Kumar <dharmendrakumar@Dharmendras-MacBook-Pro.local>
2023-12-06 11:15:24 +0530

    episode 1

---
+++ episode-1/App.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement('h1',{
+//     id:'heading',extra:'abc'
+// },'Hello World from React');
+
+const result = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(result);
---
+++ episode-1/README.md
@@ -0,0 +1,1 @@
+# Learning Namaste React
